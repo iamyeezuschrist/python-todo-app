b6d658057f651af49cd101ea11e68ede58bc20ac
iamyeezuschrist <camrymanjr@me.com>
2022-12-09 13:40:12 +0300

    'add' button created

--- functions.py
+++ functions.py
@@ -8,7 +8,7 @@ def write_todos(todos_arg, filepath=FILEPATH):
         file_local.writelines(todos_arg)
 
 
-def get_todos(filepath='FILEPATH'):
+def get_todos(filepath=FILEPATH):
     """ Read the text files and return the list of to-do items """
     with open(filepath, 'r') as file_local:
         todos_local = file_local.readlines()
